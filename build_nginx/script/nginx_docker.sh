3b6e03bf81a938cfeb002827a234d91440a231d1
陈泰任 <867804244@qq.com>
2018-09-19 02:44:17 +0800

    修复nginx配置错误

--- build_nginx/script/nginx_docker.sh
+++ build_nginx/script/nginx_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name nginx -p 80:80 -p 443:443 -v ../etc/nginx/conf.d:/etc/nginx/conf.d -v ../log/nginx:/var/log/nginx -v ../www:/var/www -d orinfy/nginx
+docker run --name nginx -p 80:80 -p 443:443 -v `pwd`/etc/nginx/conf.d:/etc/nginx/conf.d -v `pwd`/log/nginx:/var/log/nginx -v `pwd`/www:/var/www -d orinfy/nginx
--- build_redis/script/redis_docker.sh
+++ build_redis/script/redis_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name redis -p 6379:6379 -v ../data/redis:/var/lib/redis -v ../log/redis:/var/log/redis -d orinfy/redis
+docker run --name redis -p 6379:6379 -v `pwd`/data/redis:/var/lib/redis -v `pwd`/log/redis:/var/log/redis -d orinfy/redis
