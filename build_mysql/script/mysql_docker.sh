1af8e7bc359236df55c017b4a299fac8651b6bbd
陈泰任 <867804244@qq.com>
2018-09-19 02:09:26 +0800

    开启mysql慢查询

--- build_mysql/script/mysql_docker.sh
+++ build_mysql/script/mysql_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name mysql -p 3306:3306 -v ../data/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=admin888 -d orinfy/mysql
+docker run --name mysql -p 3306:3306 -v ../data/mysql:/var/lib/mysql -v ../log/mysql:/var/log/mysql -e MYSQL_ROOT_PASSWORD=admin888 -d orinfy/mysql
